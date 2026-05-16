aa0396a7bf6ec9cc6939ebfcbecac41afc239142
Briyanssss <briyannicho14@gmail.com>
2026-05-16 21:17:01 +0700

    Task 1: Update factories.js with fake products

--- tests/factories.js
+++ tests/factories.js
@@ -1,8 +1,16 @@
 const { faker } = require('@faker-js/faker');
-const { Product, Category } = require('../src/models/product');
 
-class ProductFactory {
-  
-}
+/**
+ * Membuat data Product palsu secara acak untuk keperluan testing
+ */
+const fakeProduct = () => {
+    return {
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        category: faker.commerce.department(),
+        price: parseFloat(faker.commerce.price()),
+        available: faker.datatype.boolean()
+    };
+};
 
-module.exports = { ProductFactory };
+module.exports = { fakeProduct };
